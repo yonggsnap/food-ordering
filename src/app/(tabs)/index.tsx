83b83d4e17523194abd1790f51044d91983a97fa
yonggsnap <ronald.yonggi@snappymob.com>
2024-12-24 19:08:04 +0800

    display products using FlatList

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,17 @@
-import { View } from "@/src/components/Themed";
 import products from "@/assets/data/products";
 import ProductListItem from "@/src/components/ProductListItem";
+import { FlatList } from "react-native";
 
 const product = products[0];
 
 export default function MenuScreen() {
   return (
-    <View>
-      <ProductListItem product={products[0]} />
-      <ProductListItem product={products[1]} />
-    </View>
+    <FlatList 
+    data={products}
+    renderItem={({ item }) => <ProductListItem product={item} />}
+    numColumns={2}
+    contentContainerStyle={{ padding: 10, gap: 10 }}
+    columnWrapperStyle={{ gap: 10 }}
+    />
   );
 }
