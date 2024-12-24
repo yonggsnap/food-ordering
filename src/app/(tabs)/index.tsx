34f8bbb382fc1d1a0dea5a7ca88838eb2d734a74
yonggsnap <ronald.yonggi@snappymob.com>
2024-12-24 15:48:41 +0800

    update index

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,35 +1,14 @@
-import { StyleSheet } from "react-native";
+import { View } from "@/src/components/Themed";
+import products from "@/assets/data/products";
+import ProductListItem from "@/src/components/ProductListItem";
 
-import EditScreenInfo from "@/src/components/EditScreenInfo";
-import { Text, View } from "@/src/components/Themed";
+const product = products[0];
 
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View>
+      <ProductListItem product={products[0]} />
+      <ProductListItem product={products[1]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
