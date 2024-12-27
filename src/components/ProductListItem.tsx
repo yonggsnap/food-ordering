e7a169b179f3131c1ecebce83deb0b8da0698dd7
yonggsnap <ronald.yonggi@snappymob.com>
2024-12-27 15:07:10 +0800

    fix link path add menu

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -13,7 +13,7 @@ interface ProductListItemProps {
 
 export default function ProductListItem({ product }: ProductListItemProps) {
   return (
-    <Link href={`/${product.id}`} asChild>
+    <Link href={`/menu/${product.id}`} asChild>
       <Pressable style={styles.container}>
         <Image
           style={styles.image}
